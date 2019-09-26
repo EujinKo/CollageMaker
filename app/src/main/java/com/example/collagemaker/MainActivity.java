19f392d30f58d4eb0565a0a36cba599f74877377
EujinKo <42593556+EujinKo@users.noreply.github.com>
2019-09-26 12:01:27 -0700

    added camera intent to four imageviews

--- app/src/main/java/com/example/collagemaker/MainActivity.java
+++ app/src/main/java/com/example/collagemaker/MainActivity.java
@@ -2,10 +2,18 @@ package com.example.collagemaker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static private int id_cur_imageview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,4 +21,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data){
+        if(requestCode == REQUEST_IMAGE_CAPTURE &&
+        resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView imageView = findViewById(id_cur_imageview);
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    // Set up intent for camera
+    public void captureImage(View view){
+      Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+      id_cur_imageview = view.getId();
+      startActivityForResult(cameraIntent,REQUEST_IMAGE_CAPTURE);
+    }
+
+    public void shareImage(View view){
+
+        Intent shareIntent = new Intent(
+                android.content.Intent.ACTION_SEND);
+
+
+    }
 }
